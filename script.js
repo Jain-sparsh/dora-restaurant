3e1581bbf875232edfb2fdfe0abe0cbbb1c15ca8
Jain-sparsh <93875355+Jain-sparsh@users.noreply.github.com>
2021-12-10 10:00:59 +0000

    first dummy web

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    alert("this website is non responsive, kindly view it in desktop or pc for better view");
+
+
+function contactus()
+{
+    alert('this is just a dummy restaurant website ....,');
+}
+
